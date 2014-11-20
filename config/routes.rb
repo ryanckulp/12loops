1e96eab82aedb3ae6822c14389319687211e7d36
Ryan Kulp <ryanckulp@gmail.com>
2014-11-20 02:12:16 -0500

    301 escape option

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Loops::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  match "/ESC", :to => redirect("/")
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
@@ -54,3 +56,5 @@ Loops::Application.routes.draw do
   #     resources :products
   #   end
 end
+
+
