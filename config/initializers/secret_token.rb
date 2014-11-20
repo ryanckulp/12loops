3b12f99d7ff957f1804cb21bb6c8c0be121ffa3b
Ryan Kulp <ryanckulp@gmail.com>
2014-11-20 01:54:02 -0500

    v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Loops::Application.config.secret_key_base = '281f3388c0e258e5e70c017aadcd9c592b35e80664e638382583bf79cb39235cefce613cee0888515e280741b365e3b37a9cda46a4be1c174ed06175fdb36d2b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Loops::Application.config.session_store :cookie_store, key: '_loops_session'
