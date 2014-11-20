bb0e8c71e3382a3ba11e263efc91d35020286c0b
Ryan Kulp <ryanckulp@gmail.com>
2014-11-20 02:45:41 -0500

    add thank you page

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Loops::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  match "/ESC", :to => redirect("/")
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
