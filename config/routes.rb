8a9c94e5dbf5cd15c08f0a9578b2d353e7e96997
Ryan Kulp <ryanckulp@gmail.com>
2015-05-21 12:57:58 -0400

    Volunteer page

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,12 @@ get '/map', to: 'pages#map'
 get '/thanks', to: 'pages#thanks'
 get '/help', to: 'pages#help'
 get '/scorecard', to: 'pages#scorecard'
+get '/volunteer', to: 'pages#volunteer'
+get '/thank-you', to: 'pages#thank-you'
 
 # Hotlinks
 get '/donate' => redirect("https://www.indiegogo.com/projects/12-loops-a-streetball-project/")
 
-
- end
+end
 
 
